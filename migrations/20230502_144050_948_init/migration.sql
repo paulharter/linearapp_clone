35adf1fe8ccea7de3be7eaed9fe1da1d63b500ee
Paul Harter <paul@glowinthedark.co.uk>
2023-05-02 15:51:21 +0100

    configure the local electric app

---
+++ migrations/20230502_144050_948_init/migration.sql
@@ -0,0 +1,12 @@
+/*
+ElectricSQL Migration
+name: 20230502_144050_948_init
+title: init
+
+When you build or sync these migrations we will add some triggers and metadata
+so that ElectricSQL's Satellite component can sync your data.
+
+Write your SQLite migration below.
+*/
+
+
